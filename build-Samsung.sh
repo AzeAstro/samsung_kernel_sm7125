1b4504723bf88bece12a9b622c884f3e1886094e
Atlas <46817089+AzeAstro@users.noreply.github.com>
2023-11-10 16:31:43 +0400

    Build script fixed
    Now you can run script and lay your back :D

--- build-Samsung.sh
+++ build-Samsung.sh
@@ -22,19 +22,44 @@ echo -e "${bold}Compiling with CLANG${normal}\n$KBUILD_COMPILER_STRING"
 clang_build
 if [ -f "$out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "$out/drivers/net/wireless/realtek/rtl8188eus/8188eu.ko" ] && [ -f "$out/drivers/net/wireless/realtek/rtl8812au/88XXau.ko" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
-ZIPNAME="NethutnerKernel-A525FXXS5DWD2-$(date '+%Y%m%d-%H%M').zip"
+
+# Names
+CURRENTTIME=$(date '+%Y%m%d-%H%M')
+KERNELZIPNAME="NethutnerKernel-A525FXXS5DWD2-$CURRENTTIME.zip"
+MAGISKZIPNAME="MagiskModule-A525FXXS5DWD2-$CURRENTTIME.zip"
+
+# Kernel packing
 if [ ! -d AnyKernel3 ]; then
    git clone -q https://github.com/AzeAstro/AnyKernel3.git -b samsung
 fi;
 cp -f $out/arch/arm64/boot/Image.gz-dtb AnyKernel3
 cd AnyKernel3
-zip -r9 "$HOME/$ZIPNAME" *
+zip -r9 "$HOME/$KERNELZIPNAME" *
 cd ..
-echo -e "Signing modules"
-$out/scripts/sign-file sha512 $out/certs/signing_key.pem $out/certs/signing_key.x509 $out/drivers/net/wireless/realtek/rtl8188eus/*.ko
-$out/scripts/sign-file sha512 $out/certs/signing_key.pem $out/certs/signing_key.x509 $out/drivers/net/wireless/realtek/rtl8812au/*.ko
+rm -rf AnyKernel3
+
+
+# Kernel modules of RealTek devices
+echo -e "Signing kernel modules..."
+$out/scripts/sign-file sha512 $out/certs/signing_key.pem $out/certs/signing_key.x509 $out/drivers/net/wireless/realtek/rtl8188eus/8188eu.ko
+$out/scripts/sign-file sha512 $out/certs/signing_key.pem $out/certs/signing_key.x509 $out/drivers/net/wireless/realtek/rtl8812au/88XXau.ko
+
+
+# Packing kernel modules
+echo -e "Packing kernel modules ..."
+git clone -q https://github.com/AzeAstro/MagiskKernelModuleTemplate.git
+cd MagiskKernelModuleTemplate
+rm system/lib/modules/placeholder
+cp $out/drivers/net/wireless/realtek/rtl8188eus/8188eu.ko system/lib/modules/
+cp $out/drivers/net/wireless/realtek/rtl8812au/88XXau.ko system/lib/modules/
+zip -r9 "$HOME/$MAGISKZIPNAME" *
+cd ..
+rm -rf MagiskKernelModuleTemplate
+
+# Finishing up
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
-echo -e "Zip: $ZIPNAME\n"
+echo -e "Kernel Zip: $KERNELZIPNAME"
+echo -e "Magisk module: $MAGISKZIPNAME\n"
 else
  echo -e "\nCompilation failed!\n"
 fi;
