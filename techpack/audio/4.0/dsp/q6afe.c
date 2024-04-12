c2dacdec7ab7ebc50a1ede4f2d8d4fe44342e40f
Atlas <46817089+AzeAstro@users.noreply.github.com>
2024-04-13 01:19:50 +0400

    Error Fix
    Fixed type error:
    error: format '%s' expects argument of type 'char *', but argument 2 has type 'uint32_t' [-Werror=format=]

--- techpack/audio/4.0/dsp/q6afe.c
+++ techpack/audio/4.0/dsp/q6afe.c
@@ -6141,7 +6141,7 @@ int afe_tas_smartamp_get_calib_data(uint32_t module_id, uint32_t param_id,
 	} else if (module_id == this_afe.tas_module_rx) {
 		port = port_id + 1;
 	} else {
-		pr_err("[TI-SmartPA:%s] invalid module id %d\n", module_id);
+		pr_err("[TI-SmartPA:%s] invalid module id %d\n","Error", module_id);
 		goto fail_cmd;
 	}
 	memset(&param_hdr, 0, sizeof(param_hdr));
@@ -6188,7 +6188,7 @@ int afe_tas_smartamp_set_calib_data(uint32_t module_id, uint32_t param_id,
 	} else if (module_id == this_afe.tas_module_tx) {
 		port = port_id + 1;
 	} else {
-		pr_err("[TI-SmartPA:%s] invalid module id %d\n", module_id);
+		pr_err("[TI-SmartPA:%s] invalid module id %d\n","Error", module_id);
 		return ret;
 	}
 	packed_param_size = sizeof(param_hdr) + length;
