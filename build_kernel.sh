15633d216c001512bf5cc143ffb24a390aa50aae
Atlas <46817089+AzeAstro@users.noreply.github.com>
2024-04-12 22:41:08 +0400

    Build Script added
    GCC: https://android.googlesource.com/platform/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/+/refs/tags/android-14.0.0_r0.58
    
    Clang:
    https://github.com/crdroidandroid/android_prebuilts_clang_host_linux-x86_clang-6778096

---
+++ build_kernel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+export ARCH=arm64
+mkdir out
+
+BUILD_CROSS_COMPILE=/home/atlas/kernel/AOSP13/toolchain/gcc/bin/aarch64-linux-android-
+KERNEL_LLVM_BIN=/home/atlas/kernel/AOSP13/toolchain/clang/bin/clang
+CLANG_TRIPLE=aarch64-linux-gnu-
+KERNEL_MAKE_ENV="DTC_EXT=/home/atlas/kernel/AOSP13/toolchain/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/lineage-a52q_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
+ 
+cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
