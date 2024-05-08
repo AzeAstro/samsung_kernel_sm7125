b57f0b57a589ad6e6ba57da60bc0ab9c90070a50
Atlas <46817089+AzeAstro@users.noreply.github.com>
2024-05-08 20:48:58 +0400

    Permissions fixed

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,12 +3,20 @@
 export ARCH=arm64
 mkdir out
 
-BUILD_CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-KERNEL_LLVM_BIN=$(pwd)/toolchain/llvm-arm-toolchain-ship/10.0/bin/clang
+BUILD_CROSS_COMPILE=/home/atlas_co/kernel/OneUI6Fresh/toolchain/gcc/bin/aarch64-linux-android-
+KERNEL_LLVM_BIN=/home/atlas_co/kernel/OneUI6Fresh/toolchain/clang/bin/clang
 CLANG_TRIPLE=aarch64-linux-gnu-
-KERNEL_MAKE_ENV="DTC_EXT=$(pwd)/tools/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
+KERNEL_MAKE_ENV="DTC_EXT=/home/atlas_co/kernel/OneUI6/toolchain/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a52q_eur_open_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a52q_nethunter_defconfig
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
+
+files=$(find "$out" -type f -name "*.ko")
+
+# Loop through each file found
+for file in $files; do
+    echo "Found module: $file\nSigning with key"
+    $out/scripts/sign-file sha512 $out/certs/signing_key.pem $out/certs/signing_key.x509 $file
+done
