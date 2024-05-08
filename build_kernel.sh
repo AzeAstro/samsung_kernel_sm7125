b5ccfe3e69d32265bbe3f921d7cd48e80146bcfc
Atlas <46817089+AzeAstro@users.noreply.github.com>
2024-05-08 21:22:40 +0400

    Build script modified
    Now after build, build script automatically signs kernel modules so you don't have to think for an hour about "what did I do wrong" after compiling.
    Thank me later :D

--- build_kernel.sh
+++ build_kernel.sh
@@ -2,6 +2,7 @@
 
 export ARCH=arm64
 mkdir out
+export out=out
 
 BUILD_CROSS_COMPILE=/home/atlas_co/kernel/OneUI6Fresh/toolchain/gcc/bin/aarch64-linux-android-
 KERNEL_LLVM_BIN=/home/atlas_co/kernel/OneUI6Fresh/toolchain/clang/bin/clang
@@ -11,12 +12,11 @@ KERNEL_MAKE_ENV="DTC_EXT=/home/atlas_co/kernel/OneUI6/toolchain/dtc CONFIG_BUILD
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE a52q_nethunter_defconfig
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
  
-cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
 
-files=$(find "$out" -type f -name "*.ko")
+files=$(find out -type f -name "*.ko")
 
 # Loop through each file found
 for file in $files; do
-    echo "Found module: $file\nSigning with key"
+    echo "Found module: $file. Signing with key"
     $out/scripts/sign-file sha512 $out/certs/signing_key.pem $out/certs/signing_key.x509 $file
 done
