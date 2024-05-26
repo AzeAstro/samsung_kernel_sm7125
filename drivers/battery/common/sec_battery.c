84e50fdbd3978cfc7c0d23353945ea85c9fd990e
Atlas <46817089+AzeAstro@users.noreply.github.com>
2024-05-26 21:31:52 +0400

    Error Fix

--- drivers/battery/common/sec_battery.c
+++ drivers/battery/common/sec_battery.c
@@ -295,7 +295,7 @@ static int __init read_f_mode(char *str)
 __setup("f_mode=", read_f_mode);
 #endif
 
-inline void battery_wakeup_source_init(struct device *dev, struct wakeup_source **ws, const char *name)
+void battery_wakeup_source_init(struct device *dev, struct wakeup_source **ws, const char *name)
 {
 	if (!(*ws)) {
 		*ws = wakeup_source_create(name);
