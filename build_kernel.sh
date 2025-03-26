b038bbcac8d76d8ea5e8fe377f6e26282656af8b
Atlas <46817089+AzeAstro@users.noreply.github.com>
2025-03-27 01:21:45 +0400

    Nethunter V2 and its fixes

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,7 @@ KERNEL_LLVM_BIN=/home/atlas_co/Documents/GitHub/KernelCompilers/a52q/OneUI5/clan
 CLANG_TRIPLE=aarch64-linux-gnu-
 KERNEL_MAKE_ENV="DTC_EXT=/home/atlas_co/Documents/GitHub/KernelCompilers/a52q/OneUI5/dtc CONFIG_BUILD_ARM64_DT_OVERLAY=y"
 
-make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/a52q_nethunter_defconfig
+make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE vendor/a52q_nethunterV2_defconfig
 make -j8 -C $(pwd) O=$(pwd)/out $KERNEL_MAKE_ENV ARCH=arm64 CROSS_COMPILE=$BUILD_CROSS_COMPILE REAL_CC=$KERNEL_LLVM_BIN CLANG_TRIPLE=$CLANG_TRIPLE
  
 cp out/arch/arm64/boot/Image $(pwd)/arch/arm64/boot/Image
